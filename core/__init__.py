8283df1cd78b82fde7bea4a118bfdb6325db9a39
zhangaimin <zhangaimin@focuschina.com>
2025-12-22 11:50:42 +0000

    更新子流程图

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,7 @@
 from .ppt_parser import PPTParser
 from .transcript_generator import TranscriptGenerator  
-from .speech_generation import SpeechSynthesisPlugin
-from .video_generation import VideoSynthesizer
+from .speech_generator import SpeechSynthesisPlugin
+from .video_generator import VideoSynthesizer
 from .pipeline import PPTToVideoPipeline
 
 __all__ = [
